8ddafc57ec1a7aa3e25832b57a0a9c1a9fb0cc67
Hiram Torres Lopez <is700845@iteso.mx>
2020-10-30 22:41:44 -0700

    Add mongo connection

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // App holds the Video service (seems okay)
@@ -14,13 +18,30 @@ type App struct {
 }
 
 func main() {
+	mongoString := os.Getenv("MONGO_STRING")
+
 	// Create a logrus logger and set up the output format as JSON
 	logger := logrus.New()
 	logger.Formatter = &logrus.JSONFormatter{}
 
+	// Connect to Mongo Atlas
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		mongoString,
+	))
+	if err != nil {
+		logger.Fatal(err)
+	}
+
 	// Create an app instance
 	awesome := &App{
 		logger: logger,
+		videos: NewVideoService(
+			logger,
+			mongoClient,
+		),
 	}
 
 	// Create a Gorilla Mux router
