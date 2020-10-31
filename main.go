0c2f4284a4885bde164e41d36a70f9fe683464aa
Hiram Torres Lopez <is700845@iteso.mx>
2020-10-31 12:13:37 -0700

    Update main app

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	muxgo "github.com/muxinc/mux-go"
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -14,20 +15,25 @@ import (
 // App holds the Video service (seems okay)
 type App struct {
 	logger *logrus.Logger
+	assets Assets
 	videos Videos
 }
 
 func main() {
+	// Environment variables
 	mongoString := os.Getenv("MONGO_STRING")
+	muxTokenID := os.Getenv("MUX_TOKEN_ID")
+	muxTokenSecret := os.Getenv("MUX_TOKEN_SECRET")
 
 	// Create a logrus logger and set up the output format as JSON
 	logger := logrus.New()
 	logger.Formatter = &logrus.JSONFormatter{}
 
-	// Connect to Mongo Atlas
+	// Context with timeout for establish connection with Mongo Atlas
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
+	// Connect to Mongo Atlas
 	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(
 		mongoString,
 	))
@@ -35,9 +41,20 @@ func main() {
 		logger.Fatal(err)
 	}
 
+	// Mux.com API Client Initialization
+	muxClient := muxgo.NewAPIClient(
+		muxgo.NewConfiguration(
+			muxgo.WithBasicAuth(muxTokenID, muxTokenSecret),
+		),
+	)
+
 	// Create an app instance
 	awesome := &App{
 		logger: logger,
+		assets: NewAssetService(
+			logger,
+			muxClient,
+		),
 		videos: NewVideoService(
 			logger,
 			mongoClient,
