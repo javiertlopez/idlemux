f32b09f9fd57fd752037b4c72dfbb9838d243c84
Hiram Torres Lopez <is700845@iteso.mx>
2020-10-31 11:44:55 -0700

    Add Assets definition

--- error.go
+++ error.go
@@ -4,3 +4,9 @@ import "errors"
 
 // ErrVideoNotFound definition
 var ErrVideoNotFound = errors.New("video not found")
+
+// ErrAssetNotFound definition
+var ErrAssetNotFound = errors.New("asset not found")
+
+// ErrIngestionFailed definition
+var ErrIngestionFailed = errors.New("ingestion failed")
