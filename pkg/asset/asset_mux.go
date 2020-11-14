9a9425b7db2c2d731037fcc0173781ddae4a0b6f
Hiram Torres Lopez <hiram@jtl.mx>
2020-11-14 13:36:09 -0700

    Update asset implementation

--- pkg/asset/asset_mux.go
+++ pkg/asset/asset_mux.go
@@ -2,18 +2,24 @@ package asset
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
+	"time"
 
-	awesome "github.com/javiertlopez/awesome/pkg"
+	"github.com/dgrijalva/jwt-go"
 	muxgo "github.com/muxinc/mux-go"
 	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
+
+	awesome "github.com/javiertlopez/awesome/pkg"
 )
 
 // videos struct holds the logger and MongoDB client
 type assets struct {
-	logger *logrus.Logger
-	mux    *muxgo.APIClient
+	logger    *logrus.Logger
+	mux       *muxgo.APIClient
+	keyID     string
+	keySecret string
 }
 
 type asset struct {
@@ -24,23 +30,35 @@ type asset struct {
 func NewAssetService(
 	l *logrus.Logger,
 	m *muxgo.APIClient,
+	id string,
+	secret string,
 ) awesome.Assets {
 	return &assets{
-		logger: l,
-		mux:    m,
+		logger:    l,
+		mux:       m,
+		keyID:     id,
+		keySecret: secret,
 	}
 }
 
 // Ingest send a source file url to mux.com
 // Returns a string Asset ID
-func (a *assets) Ingest(ctx context.Context, source string) (string, error) {
+func (a *assets) Ingest(ctx context.Context, source string, public bool) (string, error) {
+	var policy []muxgo.PlaybackPolicy
+
+	if public {
+		policy = append(policy, muxgo.PUBLIC)
+	} else {
+		policy = append(policy, muxgo.SIGNED)
+	}
+
 	asset, err := a.mux.AssetsApi.CreateAsset(muxgo.CreateAssetRequest{
 		Input: []muxgo.InputSettings{
-			muxgo.InputSettings{
+			{
 				Url: source,
 			},
 		},
-		PlaybackPolicy: []muxgo.PlaybackPolicy{muxgo.PUBLIC},
+		PlaybackPolicy: policy,
 	})
 
 	if err != nil {
@@ -68,11 +86,85 @@ func (a *assets) GetByID(ctx context.Context, id string) (*awesome.Asset, error)
 		data: response.Data,
 	}
 
-	return body.toModel(), nil
+	asset := body.toModel()
+
+	if len(body.data.PlaybackIds) > 0 {
+		var source, poster, thumbnail string
+		playbackID := body.data.PlaybackIds[0].Id
+
+		switch body.data.PlaybackIds[0].Policy {
+		case muxgo.PUBLIC:
+			source = fmt.Sprintf(
+				"https://stream.mux.com/%s.m3u8",
+				playbackID,
+			)
+
+			poster = fmt.Sprintf(
+				"https://image.mux.com/%s/thumbnail.png?width=%s&height=%s&smart_crop=true&time=%s",
+				playbackID,
+				"1920",
+				"1080",
+				"7",
+			)
+
+			thumbnail = fmt.Sprintf(
+				"https://image.mux.com/%s/thumbnail.png?width=%s&height=%s&smart_crop=true&time=%s",
+				playbackID,
+				"640",
+				"360",
+				"7",
+			)
+		case muxgo.SIGNED:
+			token, err := a.signURL(playbackID, "v", asset.Duration, 0, 0)
+			if err != nil {
+				return nil, err
+			}
+
+			source = fmt.Sprintf(
+				"https://stream.mux.com/%s.m3u8?token=%s",
+				playbackID,
+				token,
+			)
+
+			token, err = a.signURL(playbackID, "t", asset.Duration, 1920, 1080)
+			if err != nil {
+				return nil, err
+			}
+
+			poster = fmt.Sprintf(
+				"https://image.mux.com/%s/thumbnail.png?token=%s",
+				playbackID,
+				token,
+			)
+
+			token, err = a.signURL(playbackID, "t", asset.Duration, 640, 360)
+			if err != nil {
+				return nil, err
+			}
+
+			thumbnail = fmt.Sprintf(
+				"https://image.mux.com/%s/thumbnail.png?token=%s",
+				playbackID,
+				token,
+			)
+		}
+
+		asset.Poster = poster
+		asset.Thumbnail = thumbnail
+		asset.Sources = []awesome.Source{
+			{
+				ID:     playbackID,
+				Source: source,
+				Type:   "application/x-mpegURL",
+			},
+		}
+	}
+
+	return asset, nil
 }
 
 func (a *asset) toModel() *awesome.Asset {
-	response := &awesome.Asset{
+	return &awesome.Asset{
 		ID:                  a.data.Id,
 		CreatedAt:           a.data.CreatedAt,
 		Status:              a.data.Status,
@@ -82,37 +174,50 @@ func (a *asset) toModel() *awesome.Asset {
 		AspectRatio:         a.data.AspectRatio,
 		Passthrough:         a.data.Passthrough,
 	}
+}
 
-	if len(a.data.PlaybackIds) > 0 {
-		playbackID := a.data.PlaybackIds[0].Id
-
-		response.Poster = fmt.Sprintf(
-			"https://image.mux.com/%s/thumbnail.png?width=%s&height=%s&smart_crop=true&time=%s",
-			playbackID,
-			"1920",
-			"1080",
-			"7",
-		)
-
-		response.Thumbnail = fmt.Sprintf(
-			"https://image.mux.com/%s/thumbnail.png?width=%s&height=%s&smart_crop=true&time=%s",
-			playbackID,
-			"640",
-			"360",
-			"7",
-		)
-
-		response.Sources = []awesome.Source{
-			awesome.Source{
-				ID: playbackID,
-				Source: fmt.Sprintf(
-					"https://stream.mux.com/%s.m3u8",
-					playbackID,
-				),
-				Type: "application/x-mpegURL",
-			},
-		}
+func (a *assets) signURL(
+	playbackID string,
+	audience string,
+	duration float64,
+	width int,
+	height int,
+) (string, error) {
+	decodedKey, err := base64.StdEncoding.DecodeString(a.keySecret)
+	if err != nil {
+		return "", err
 	}
 
-	return response
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(decodedKey)
+	if err != nil {
+		return "", err
+	}
+
+	round := int(duration * 1.6)
+
+	claims := jwt.MapClaims{
+		"sub": playbackID,
+		"aud": audience,
+		"exp": time.Now().Add(time.Second * time.Duration(round)).Unix(),
+		"kid": a.keyID,
+	}
+
+	if audience == "t" {
+		claims["time"] = 7
+		claims["width"] = width
+		claims["height"] = height
+		claims["smart_crop"] = true
+	}
+
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodRS256,
+		claims,
+	)
+
+	tokenString, err := token.SignedString(signKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
 }
