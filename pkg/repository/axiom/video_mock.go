814408d92cd83e2ea0db2f1058f07362e319dcaa
Hiram Torres Lopez <hiram@jtl.mx>
2020-12-05 15:26:35 -0700

    Add controller test

--- pkg/repository/axiom/video_mock.go
+++ pkg/repository/axiom/video_mock.go
@@ -34,19 +34,19 @@ func (m *MockedVideos) Create(ctx context.Context, anyVideo model.Video) (model.
 }
 
 // GetByID mocked method
-func (m *MockedVideos) GetByID(ctx context.Context, id string) (*model.Video, error) {
+func (m *MockedVideos) GetByID(ctx context.Context, id string) (model.Video, error) {
 	ID := "fcdf5f4e-b086-4b52-8714-bf3623186185"
 	IDWithSourceFile := "a9200233-9b62-489c-9cbc-bb37f2922804"
 
 	switch id {
 	case ID:
-		return &model.Video{
+		return model.Video{
 			ID:          &ID,
 			Title:       "Some Might Say",
 			Description: "Oasis song from (What's the Story) Morning Glory? album.",
 		}, nil
 	case IDWithSourceFile:
-		return &model.Video{
+		return model.Video{
 			ID:          &IDWithSourceFile,
 			Title:       "Some Might Say",
 			Description: "Oasis song from (What's the Story) Morning Glory? album.",
@@ -56,5 +56,5 @@ func (m *MockedVideos) GetByID(ctx context.Context, id string) (*model.Video, er
 		}, nil
 	}
 
-	return nil, errorcodes.ErrVideoNotFound
+	return model.Video{}, errorcodes.ErrVideoNotFound
 }
