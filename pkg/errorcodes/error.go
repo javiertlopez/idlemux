c2d01211e1133ce0cdf22ab697af80e96b174e90
Hiram Torres Lopez <hiram@jtl.mx>
2020-12-05 14:47:01 -0700

    Add errorcodes

---
+++ pkg/errorcodes/error.go
@@ -0,0 +1,12 @@
+package errorcodes
+
+import "errors"
+
+// ErrVideoNotFound definition
+var ErrVideoNotFound = errors.New("video not found")
+
+// ErrAssetNotFound definition
+var ErrAssetNotFound = errors.New("asset not found")
+
+// ErrIngestionFailed definition
+var ErrIngestionFailed = errors.New("ingestion failed")
