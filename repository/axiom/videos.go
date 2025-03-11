4ed34ec54b457f40d78366ea661bc5168fd7731c
Hiram Torres <hiram@jtl.mx>
2025-03-10 21:22:46 -0600

    upgrade implementation

--- repository/axiom/videos.go
+++ repository/axiom/videos.go
@@ -6,8 +6,8 @@ import (
 
 	guuid "github.com/google/uuid"
 	"github.com/sirupsen/logrus"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 
 	"github.com/javiertlopez/awesome/errorcodes"
 	"github.com/javiertlopez/awesome/model"
